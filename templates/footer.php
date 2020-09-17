d2293f1a7d64b94ffd2904a37cd191b6943f77b1
cattsas <cattsas1980@gmail.com>
2020-09-17 20:14:37 -0300

    Agregue footer y table.php

---
+++ templates/footer.php
@@ -0,0 +1,44 @@
+</main>
+
+<footer>
+
+<div class="row">
+
+    <div class="col-md-4 order-1 row align-items-center">
+        <h5><a class="sinsub" href="mailto:contacto@newtech.com.ar"> Contacto a nuestro correo electrónico </a> </h5>
+    </div>
+
+    <div class="col-md-4 order-2 row align-items-center">
+
+        <h5 class="seguinos_redes"> Seguinos en redes</h5>
+
+
+        <ul class="d-flex pl-0 pt-3">
+
+            <div class="align-self-end formato_logo_redes">
+                <li>
+                    <a href="https://www.facebook.com/" target="_blank"> </a>
+                </li>
+            </div>
+
+            <div class="formato_logo_redes">
+                <li>
+                    <a href="https://www.instagram.com/" target="_blank"> </a>
+                </li>
+            </div>
+
+            <div class="formato_logo_redes">
+                <li>
+                    <a href="https://api.whatsapp.com/send?phone=+5492983407560" target="_blank"> </a>
+                </li>
+            </div>
+
+        </ul>
+    </div>
+    <div class="col-md-4 order-3 row align-items-center">
+        <p class="text-center "> New Tech &copy; - Año 2020 - </p>
+        <p class="text-center"> Todos los derechos reservados </p>
+    </div>
+</div>
+
+</footer>
