2e2834fc271dbca4459e7b6065de9ba9b45dc2fe
JuanMolfese <juanjosemolfese@gmail.com>
2020-09-20 14:31:16 -0300

    se elimino classes de css original del footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -2,43 +2,42 @@
 
 <footer>
 
-<div class="row">
+    <div class="row">
 
-    <div class="col-md-4 order-1 row align-items-center">
-        <h5><a class="sinsub" href="mailto:contacto@newtech.com.ar"> Contacto a nuestro correo electrónico </a> </h5>
+        <div class="col-md-4 order-1 row align-items-center">
+            <h5><a href="mailto:contacto@newtech.com.ar"> Contacto a nuestro correo electrónico </a> </h5>
+        </div>
+
+        <div class="col-md-4 order-2 row align-items-center">
+
+            <h5> Seguinos en redes</h5>
+
+            <ul class="d-flex pl-0 pt-3">
+
+                <div class="align-self-end">
+                    <li>
+                        <a href="https://www.facebook.com/" target="_blank"> </a>
+                    </li>
+                </div>
+
+                <div>
+                    <li>
+                        <a href="https://www.instagram.com/" target="_blank"> </a>
+                    </li>
+                </div>
+
+                <div>
+                    <li>
+                        <a href="https://api.whatsapp.com/send?phone=+5492983407560" target="_blank"> </a>
+                    </li>
+                </div>
+
+            </ul>
+        </div>
+        <div class="col-md-4 order-3 row align-items-center">
+            <p class="text-center "> New Tech &copy; - Año 2020 - </p>
+            <p class="text-center"> Todos los derechos reservados </p>
+        </div>
     </div>
-
-    <div class="col-md-4 order-2 row align-items-center">
-
-        <h5 class="seguinos_redes"> Seguinos en redes</h5>
-
-
-        <ul class="d-flex pl-0 pt-3">
-
-            <div class="align-self-end formato_logo_redes">
-                <li>
-                    <a href="https://www.facebook.com/" target="_blank"> </a>
-                </li>
-            </div>
-
-            <div class="formato_logo_redes">
-                <li>
-                    <a href="https://www.instagram.com/" target="_blank"> </a>
-                </li>
-            </div>
-
-            <div class="formato_logo_redes">
-                <li>
-                    <a href="https://api.whatsapp.com/send?phone=+5492983407560" target="_blank"> </a>
-                </li>
-            </div>
-
-        </ul>
-    </div>
-    <div class="col-md-4 order-3 row align-items-center">
-        <p class="text-center "> New Tech &copy; - Año 2020 - </p>
-        <p class="text-center"> Todos los derechos reservados </p>
-    </div>
-</div>
 
 </footer>
